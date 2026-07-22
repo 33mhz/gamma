e165dd535574116164c82027512f1edf8cd8cff2
33MHz <ing@s3rv.com>
2026-07-21 22:58:54 -0500

    post search order id

--- app/src/main/java/io/pnut/gamma/domain/model/params/single/SearchPostParam.kt
+++ app/src/main/java/io/pnut/gamma/domain/model/params/single/SearchPostParam.kt
@@ -1,9 +1,13 @@
 package io.pnut.gamma.domain.model.params.single
 
-data class SearchPostParam(val keyword: String) : BaseParam {
+data class SearchPostParam(
+    val keyword: String,
+    val order: String = "id"
+) : BaseParam {
     override fun toMap(): Map<String, String> {
         return hashMapOf(
-            "q" to keyword
+            "q" to keyword,
+            "order" to order
         )
     }
 }
