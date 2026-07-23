52a7f64ea7c8b32b68e8e6f57aa4c30328f02eac
33MHz <ing@s3rv.com>
2026-07-23 15:52:35 -0500

    fix linkabletextview

---
+++ app/src/main/java/io/pnut/gamma/presentation/view/LinkableTextView.kt
@@ -0,0 +1,11 @@
+package io.pnut.gamma.presentation.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class LinkableTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.textViewStyle
+) : AppCompatTextView(context, attrs, defStyleAttr)
