a8e0f008913bbaae9f07c3bd3bac9b8fbf36a162
33MHz <ing@s3rv.com>
2026-09-19 08:51:15 -0500

    more dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@
 plugins {
     id("com.android.application") version "9.4.1" apply false
     id("com.android.library") version "9.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
     id("com.google.dagger.hilt.android") version "2.60.1" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
     id("com.google.android.gms.oss-licenses-plugin") version "0.13.0" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
-    id("com.google.firebase.crashlytics") version "3.0.7" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
+    id("com.google.firebase.crashlytics") version "3.0.8" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.10.1" apply false
 }
 
 val kotlinVersion = project.property("kotlinVersion") as String
